151d4bf8d566616475cb20494ff2812f8681f1e2
zhan <42181249@qq.com>
2021-02-07 15:14:32 +0800

    删除排序数组中的重复项

--- source/enjoy-learning/src/main/java/leetcode/array/RemoveDuplicates.java
+++ source/enjoy-learning/src/main/java/leetcode/array/RemoveDuplicates.java
@@ -13,14 +13,26 @@ package leetcode.array;
  */
 public class RemoveDuplicates {
     public static void main(String[] args) {
-        Solution solution = new RemoveDuplicates().new Solution();
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
-        int i1 = solution.removeDuplicates(nums);
-        for (int i : nums) {
+//        Solution solution = new RemoveDuplicates().new Solution();
+//        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+//        int i1 = solution.removeDuplicates(nums);
+//        for (int i : nums) {
+//            System.out.print(i + " ");
+//        }
+
+        Solution1 solution1 = new RemoveDuplicates().new Solution1();
+        int[] nums1 = {1,1,1,2,2,3};
+        int i2 = solution1.removeDuplicates(nums1);
+        for (int i : nums1) {
             System.out.print(i + " ");
         }
     }
 
+    /**
+     * 给定数组 nums = [1,1,2],
+     * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+     * 你不需要考虑数组中超出新长度后面的元素。
+     */
     class Solution {
         public int removeDuplicates(int[] nums) {
             if (nums.length == 0) return 0;
@@ -32,6 +44,24 @@ public class RemoveDuplicates {
                 }
             }
             return i+1;
+        }
+    }
+
+    /**
+     * 给定一个增序排列数组 nums ，你需要在 原地 删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
+     * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+     * 输入：nums = [1,1,1,2,2,3]
+     * 输出：5, nums = [1,1,2,2,3]
+     */
+    class Solution1 {
+        public int removeDuplicates(int[] nums) {
+            int i = 0;
+            for (int n : nums) {
+                if (i < 2 || n > nums[i-2]) {
+                    nums[i++] = n;
+                }
+            }
+            return i;
         }
     }
 }
