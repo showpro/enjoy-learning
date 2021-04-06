cce73118732208fbe8e06294aaa7270f8da31671
zhanzhan <zhanzhan@wahaha.com.cn>
2021-04-06 10:55:37 +0800

    集合求差集

--- enjoy-learning/src/main/java/java8/Stream.java
+++ enjoy-learning/src/main/java/java8/Stream.java
@@ -15,6 +15,25 @@ import java.util.stream.Collectors;
  */
 public class Stream {
     public static void main(String[] args) {
+
+        List<String> list1 = new ArrayList();
+        list1.add("1111");
+        list1.add("2222");
+        list1.add("3333");
+
+        List<String> list2 = new ArrayList();
+        list2.add("3333");
+        //list2.add("4444");
+        //list2.add("5555");
+
+        // 差集 (list1 - list2)
+        List<String> reduce1 = list1.stream().filter(item -> !list2.contains(item)).collect(Collectors.toList());
+        reduce1.forEach(System.out::println);
+        System.out.println("-----------------");
+        // 差集 (list2 - list1)
+        List<String> reduce2 = list2.stream().filter(item -> !list1.contains(item)).collect(Collectors.toList());
+        reduce2.forEach(System.out::println);
+
         filter();
         sorted();
         map();
